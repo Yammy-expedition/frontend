379bb2ecd2fd8584cba14bde95fdb0178064383e
shu07002 <shu07002@gmail.com>
2024-11-01 14:25:24 +0900

    init: test pr preview

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">this is header</header>
+      <div>and lets check out does pr previewing work well</div>
       <Router />
     </div>
   );
