6653b482b8757219f07c0a4dd8aa978fb26077b6
shu07002 <shu07002@gmail.com>
2024-11-02 18:56:17 +0900

    feat: 사이드바 UI구현과 열리는 박스 애니메이션 구현

---
+++ src/components/main/TitleBox.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function TitleBox() {
+  return (
+    <LogoBox>
+      <BackgroundCircle />
+      <div style={{ zIndex: 10, width: '100%' }}>
+        <ServiceName>unicon</ServiceName>
+        <UnivName>for Sogang University.</UnivName>
+      </div>
+    </LogoBox>
+  );
+}
+
+const LogoBox = styled.div`
+  padding: 3rem 0 5rem 0px;
+  position: relative;
+  width: 100%;
+  height: 9.5rem;
+  display: flex;
+  justify-content: center;
+`;
+const BackgroundCircle = styled.div`
+  position: absolute;
+  width: 9.5rem;
+  height: 9.5rem;
+  background-image: linear-gradient(to top left, #888888, #ffffff, #ffffff);
+  border-radius: 10rem;
+`;
+
+const ServiceName = styled.h1`
+  text-align: center;
+  color: green;
+  font-size: 5.6rem;
+  font-family: 'Tahoma', sans-serif;
+`;
+
+const UnivName = styled.h2`
+  width: 97%;
+  text-align: right;
+  font-size: 1.6rem;
+  font-family: 'Tahoma', sans-serif;
+`;
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,5 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import TitleBox from 'components/main/TitleBox';
+import MenuGroup from 'components/main/MenuGroup';
 
 export default function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <MainPageComponents>
+      <SideBar>
+        <div>
+          <TitleBox></TitleBox>
+          <MenuGroup></MenuGroup>
+        </div>
+
+        <Footer>
+          <div>chat</div>
+          <div>|</div>
+          <div>mypage</div>
+        </Footer>
+      </SideBar>
+      <MapBox>지도</MapBox>
+    </MainPageComponents>
+  );
 }
+
+const MainPageComponents = styled.div`
+  display: flex;
+  height: 100%;
+`;
+//------------------------------------------------------
+const SideBar = styled.div`
+  z-index: 10;
+  width: 37.8rem;
+  background: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0.5rem 0 1rem rgba(0, 0, 0, 0.1);
+`;
+//------------------------------------------------------
+//------------------------------------------------------
+
+const MapBox = styled.div`
+  background: yellow;
+  width: 50rem;
+  height: 100vh;
+`;
+
+const Footer = styled.footer`
+  margin: 4rem 0;
+  display: flex;
+  gap: 0.5rem;
+  font-size: 2rem;
+`;
