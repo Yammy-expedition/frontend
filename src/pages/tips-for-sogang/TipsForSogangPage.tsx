06b7aa772d693f7455593356d8ae36cc97377b9c
Kimbyul88 <byul88byul@gmail.com>
2024-11-11 15:38:57 +0900

    fix: etc .... 사진 추가 등

--- src/pages/tips-for-sogang/TipsForSogangPage.tsx
+++ src/pages/tips-for-sogang/TipsForSogangPage.tsx
@@ -52,75 +52,73 @@ export default function TipsForSogangPage() {
 }
 
 const Wrapper = styled.main`
-  /* background-color: aquamarine; */
   width: 100%;
   height: 100vh;
   padding-left: 3rem;
 `;
 
 const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
   padding-top: 5rem;
   padding-bottom: 3rem;
-  background-color: aliceblue;
   div {
     font-size: 4rem;
   }
   p {
     font-size: 2rem;
+    color: var(--secondary-text);
   }
 `;
 
 const Nav = styled.nav`
-  background-color: bisque;
   padding: 2rem 0;
   display: flex;
   gap: 2rem;
   div {
-    background-color: #d9d9d9;
-    color: #717171;
+    background-color: var(--main-gray);
+    color: var(--secondary-text);
     border-radius: 6.5rem;
     padding: 1.5rem 3rem;
-    font-size: 2rem;
+    font-size: 1.6rem;
     cursor: pointer;
     &:hover {
-      background-color: black;
-      color: white;
+      background-color: var(--hover-bg);
+      color: var(--hover-text);
     }
   }
 `;
 
 const LocationTab = styled.ul`
-  padding: 0;
-  background-color: cadetblue;
   display: flex;
   gap: 1rem;
   list-style: none;
-  font-size: 2rem;
+  font-size: 1.6rem;
   li {
-    background-color: #d9d9d9;
+    background-color: var(--main-gray);
+    color: var(--main-text);
     border-radius: 6.5rem;
     padding: 1rem 3rem;
     cursor: pointer;
     &:hover {
-      color: white;
+      color: var(--hover-text);
       background-color: var(--primary-color);
     }
   }
 `;
 
 const PostList = styled.ul`
-  border-top: 1px solid #979797;
-  padding: 0;
+  margin-top: 3rem;
+  border-top: 1px solid var(--border-color);
   li {
     display: flex;
     padding: 2% 0;
     list-style: none;
-    border-bottom: 1px solid #979797;
+    border-bottom: 1px solid var(--border-color);
     figure {
-      margin: 0;
       width: 20rem;
       height: 20rem;
-      background-color: black;
     }
     figcaption {
       background-color: darkorange;
