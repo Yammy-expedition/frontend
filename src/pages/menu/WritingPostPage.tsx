3fc52a18d430792b98ed62e342e073ed35e54917
shu07002 <shu07002@gmail.com>
2024-11-24 16:54:10 +0900

    feat: 글쓰기 이미지 리사이즈 구현

--- src/pages/menu/WritingPostPage.tsx
+++ src/pages/menu/WritingPostPage.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import ReactQuill from 'react-quill-new';
+import ReactQuill, { Quill } from 'react-quill-new';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { Posting } from 'types/posting';
 import 'react-quill/dist/quill.snow.css';
 import { postPosting } from 'utils/postPosting';
+import { ImageResize } from 'quill-image-resize-module-ts';
+
+Quill.register('modules/ImageResize', ImageResize);
 
 export default function WritingPostPage() {
   const location = useLocation();
@@ -23,6 +26,26 @@ export default function WritingPostPage() {
         state: { boardType: state.boardType.name, posting: result }
       });
     });
+  };
+
+  useEffect(() => {
+    console.log(content);
+  }, [content]);
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, false] }], // 헤더
+      [{ align: [] }], // 정렬
+      ['bold', 'italic', 'underline', 'strike'], // 텍스트 포맷
+      [{ list: 'ordered' }, { list: 'bullet' }], // 리스트
+      ['blockquote', 'code-block'], // 인용구, 코드 블록
+      [{ color: [] }, { background: [] }], // 텍스트 색상, 배경색
+      ['link', 'image'], // 링크, 이미지, 비디오
+      ['clean'] // 포맷 초기화
+    ],
+    ImageResize: {
+      parchment: Quill.import('parchment'),
+      modules: ['Resize', 'DisplaySize']
+    }
   };
 
   return (
@@ -44,6 +67,7 @@ export default function WritingPostPage() {
         theme="snow"
         value={content}
         onChange={setContent}
+        modules={modules}
         placeholder="Please write content"
       />
 
