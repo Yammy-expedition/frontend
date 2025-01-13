1315371bd0b9d48a1fa4ae1068e295f49377c085
shu07002 <shu07002@gmail.com>
2025-01-13 15:52:54 +0900

    feat: PWA test

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
@@ -13,6 +14,14 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+console.log('Service Worker 등록을 시도합니다.');
+
+console.log(serviceWorkerRegistration);
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
